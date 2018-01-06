5760dbe7ecf044e8573042603e29a20cdd80649b
Andrey Bilokon <a.bilokon@searchinform.ru>
2018-01-06 17:45:24 +0300

    Change frequency display to 7-segment led-style digits. Show 10x Hz. Remove unused fonts. Code size 28416 bytes, data size 1005 bytes

--- Syntez/disp_ILI9341.cpp
+++ Syntez/disp_ILI9341.cpp
@@ -12,12 +12,8 @@
 #include <PDQ_ILI9341.h>      // PDQ: Hardware-specific driver library
 
 #ifdef GFX_FONT_PACKED
-#include "font\Gadugi58.pck.h"
-#include "font\Tahoma28.pck.h"
 #include "font\Tahoma18.pck.h"
 #else
-#include "font\Gadugi58.h"
-#include "font\Tahoma28.h"
 #include "font\Tahoma18.h"
 #endif
 
@@ -31,7 +27,6 @@ PDQ_ILI9341 tft;
 
 long cur_freq=0;
 long cur_freq2=0;
-char cur_freq_buf[9];
 int cur_vfo_idx=-1;
 byte cur_sideband=0xff;
 byte cur_split=0xff;
@@ -58,7 +53,6 @@ void Display_ILI9341_SPI::reset()
 {
   cur_freq=0;
   cur_freq2=0;
-  cur_freq_buf[0]=0;
   cur_vfo_idx=-1;
   cur_sideband=0xff;
   cur_split=0xff;
@@ -75,52 +69,83 @@ void Display_ILI9341_SPI::reset()
   last_tmtm=0;
 }
 
-void fmt231(char *buf, long v)
-{
-  buf[8] = 0;
-  buf[7] = '0'+v%10; v/=10;
-  buf[6] = '.';
-  buf[5] = '0'+v%10; v/=10;
-  buf[4] = '0'+v%10; v/=10;
-  buf[3] = '0'+v%10; v/=10;
-  buf[2] = '.';
-  buf[1] = '0'+v%10; v/=10;
-  if (v > 0) buf[0] = '0'+v;
-  else buf[0] = '!';
+/* Routine to Draw Large 7-Segment formated number with Arduino TFT Library
+   Contributed by William Zaggle  (Uses TFT Library DrawLine and Fill Rectangle functions).
+   https://forum.arduino.cc/index.php?topic=307348.0
+
+   int n - The number to be displayed
+   int xLoc = The x location of the upper left corner of the number
+   int yLoc = The y location of the upper left corner of the number
+   int cS = The size of the number.
+   fC is the foreground color of the number
+   bC is the background color of the number (prevents having to clear previous space)
+   nD is the number of digit spaces to occupy (must include space for minus sign for numbers < 0).
+
+    // Example to draw the number 37 in four directions in four corners of the display
+    draw7Number(37,10,10,4, ILI9341_WHITE , ILI9341_BLACK,2);          //LEFT2RIGHT
+    draw7Number90(37,10,310,4, ILI9341_WHITE , ILI9341_BLACK,2);     //DOWN2UP
+    draw7Number180(37,230,310,4, ILI9341_WHITE , ILI9341_BLACK,2);  //RIGHT2LEFT
+    draw7Number270(37,230,10,4, ILI9341_WHITE , ILI9341_BLACK,2);    //UP2DOWN
+*/
+void draw7Number(int n, int xLoc, int yLoc, byte cS, color_t fC, int bC, byte nD) {
+  int num=abs(n),i,s,t,w,col,h,a,b,si=0,j=1,d=0,S2=5*cS,S3=2*cS,S4=7*cS,x1=cS+1,x2=S3+S2+1,y1=yLoc+x1,y3=yLoc+S3+S4+1;
+  int seg[7][3]={{x1,yLoc,1},{x2,y1,0},{x2,y3+x1,0},{x1,(2*y3)-yLoc,1},{0,y3+x1,0},{0,y1,0},{x1,y3,1}};
+  byte nums[12]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x67,0x00,0x40},c=(c=abs(cS))>10?10:(c<1)?1:c,cnt=(cnt=abs(nD))>10?10:(cnt<1)?1:cnt;
+  for (xLoc+=cnt*(d=S2+(3*S3)+2);cnt>0;cnt--){
+    for (i=(num>9)?num%10:((!cnt)&&(n<0))?11:((nD<0)&&(!num))?10:num,xLoc-=d,num/=10,j=0;j<7;++j){
+      col=(nums[i]&(1<<j))?fC:bC;
+      if (seg[j][2])for(w=S2,t=seg[j][1]+S3,h=seg[j][1]+cS,a=xLoc+seg[j][0]+cS,b=seg[j][1];b<h;b++,a--,w+=2)tft.drawFastHLine(a,b,w,col);
+      else for(w=S4,t=xLoc+seg[j][0]+S3,h=xLoc+seg[j][0]+cS,b=xLoc+seg[j][0],a=seg[j][1]+cS;b<h;b++,a--,w+=2)tft.drawFastVLine(b,a,w,col);
+      for (;b<t;b++,a++,w-=2)seg[j][2]?tft.drawFastHLine(a,b,w,col):tft.drawFastVLine(b,a,w,col);
+    }
+  }
 }
 
 void drawFreq(int x, int y, long f, color_t c)
 {
-  char bbuf[9];
-  char* buf=bbuf;
-  char *oldbuf=cur_freq_buf;
-  int w=0;
-  fmt231(bbuf,f);
-  while (*buf && (*buf == *oldbuf)) {
-    if (*buf == '.') w+=17;
-    else w+=42;
-    oldbuf++;
-    buf++;
-  }
-  if (*buf) {
-    tft.setTextColor(c);
-    tft.setFont(&Gadugi58);
-    tft.fillRect(x+w,y-74,286-w,66,ILI9341_BLACK);
-    tft.setCursor(x+w,y);
-    tft.print(buf);
-    while (*oldbuf++=*buf++);
+  const int DotWidth = 24;
+  const int cS = 3;
+  const int NumWidth = 9*cS+2;
+  const int NumHeight = 20*cS+4;
+  const int SpaceWidth = (NumWidth+3)/4;
+  const int NumWidth2 = 9*(cS-1)+2;
+  const int SpaceWidth2 = (NumWidth2+3)/4;
+  const byte Space[7] = {NumWidth2+SpaceWidth2, NumWidth2+DotWidth, NumWidth+SpaceWidth, NumWidth+SpaceWidth, NumWidth+DotWidth, NumWidth+SpaceWidth, NumWidth+SpaceWidth};
+  int xx=x+4*SpaceWidth+2*DotWidth+5*NumWidth+2*NumWidth2;
+  int v;
+  long f2 = cur_freq;
+  for (byte i=0; i <= 6; i++) {
+    v=f%10; f/=10;
+    if (v != f2%10 || cur_freq == 0) {
+      if (i <= 1) 
+        draw7Number(v,xx,y,cS-1, c, ILI9341_BLACK, 1);
+      else if (i == 6 && v == 0)
+        draw7Number(v,xx,y,cS, ILI9341_BLACK, ILI9341_BLACK, 1);
+      else
+        draw7Number(v,xx,y,cS, c, ILI9341_BLACK, 1);
+    }
+    f2/=10;
+    xx-=Space[i];
   }
 }
 
-void drawFreq2(int x, int y, long f2, color_t c)
+void drawFreq2(int x, int y, long v, color_t c)
 {
-  char buf[9];
-  fmt231(buf,f2);
-  tft.fillRect(x,y-36,x+20*6+11*2,32,ILI9341_BLACK);
-  tft.setTextColor(c);
-  tft.setFont(&Tahoma28);
+  char buf[10];
+  buf[9] = 0;
+  for (byte i=8; i > 0; ) {
+    buf[i--] = '0'+v%10; 
+    v/=10;
+    if (i == 6 || i == 2) buf[i--] = '.';
+  }
+  if (v > 0) buf[0] = '0'+v;
+  else buf[0] = ' ';
+  tft.setTextColor(c,ILI9341_BLACK);
+  tft.setFont(NULL);
+  tft.setTextSize(2);
   tft.setCursor(x,y);
   tft.print(buf);
+  tft.setTextSize(1);
 }
 
 void drawBtn(int x, int y, uint8_t w, uint8_t h, const char *title, color_t cframe, color_t ctext)
@@ -149,14 +174,14 @@ void drawBtn(int x, int y, uint8_t w, uint8_t h, const char *title, color_t cfra
 }
 
 void Display_ILI9341_SPI::Draw(TRX& trx) {
-  const int FREQ_Y = 86;
+  const int FREQ_Y = 83;
   const int FREQ_H = 105; 
   const int BTN_Y = 204;
   const int SMetr_Y = 42;
   const int SMetr_H = 12;
   uint8_t vfo_idx = trx.GetVFOIndex();
-  long f = (trx.state.VFO[vfo_idx]+50) / 100;
-  long f2 = (trx.state.VFO[vfo_idx^1]+50) / 100;
+  long f = (trx.state.VFO[vfo_idx]+5) / 10;
+  long f2 = (trx.state.VFO[vfo_idx^1]+5) / 10;
 
   tft.setTextSize(1);
   
@@ -204,29 +229,29 @@ void Display_ILI9341_SPI::Draw(TRX& trx) {
   if (cur_vfo_idx != vfo_idx) {
     cur_freq=0;
     cur_freq2=0;
-    cur_freq_buf[0]=0;
+//    cur_freq_buf[0]=0;
     cur_vfo_idx=vfo_idx;
     tft.fillRect(20,FREQ_Y,300,FREQ_H,ILI9341_BLACK);
     if (cur_rit) {
-      tft.fillRect(10,(cur_vfo_idx ? FREQ_Y+82 : FREQ_Y+2),120,15,ILI9341_BLACK);
+      tft.fillRect(10,(cur_vfo_idx ? FREQ_Y+82 : FREQ_Y+4),90,16,ILI9341_BLACK);
       cur_ritval=0xffff;
     }
   }
   
   if (f != cur_freq) {
-    cur_freq=f;
     if (vfo_idx == 0)
-      drawFreq(20,FREQ_Y+75,f,ILI9341_YELLOW);
+      drawFreq(20,FREQ_Y,f,ILI9341_YELLOW);
     else
-      drawFreq(20,FREQ_Y+115,f,ILI9341_YELLOW);
+      drawFreq(20,FREQ_Y+40,f,ILI9341_YELLOW);
+    cur_freq=f;
   }
 
   if (f2 != cur_freq2) {
-    cur_freq2=f2;
     if (vfo_idx == 0)
-      drawFreq2(158,FREQ_Y+105,f2,ILI9341_CYAN);
+      drawFreq2(200,FREQ_Y+82,f2,ILI9341_CYAN);
     else
-      drawFreq2(158,FREQ_Y+37,f2,ILI9341_CYAN);
+      drawFreq2(200,FREQ_Y+4,f2,ILI9341_CYAN);
+    cur_freq2=f2;
   }
 
   if (trx.state.AttPre != cur_attpre) {
@@ -256,38 +281,37 @@ void Display_ILI9341_SPI::Draw(TRX& trx) {
       cur_ritval=0xffff;
     } else {
       drawBtn(169,BTN_Y,66,36,"RIT",ILI9341_BLACK,ILI9341_DARKGRAY);
-      tft.fillRect(10,(cur_vfo_idx ? FREQ_Y+2 : FREQ_Y+82),120,15,ILI9341_BLACK);
+      tft.fillRect(10,(cur_vfo_idx ? FREQ_Y+4 : FREQ_Y+82),90,16,ILI9341_BLACK);
     }
   }
   
   if (cur_rit && trx.RIT_Value != cur_ritval) {
     int y,v=trx.RIT_Value;
-    char buf[16];
+    char buf[8];
     if (cur_vfo_idx == 0) y=FREQ_Y+82;
-    else y=FREQ_Y+2;
+    else y=FREQ_Y+4;
     cur_ritval=trx.RIT_Value;
-    tft.fillRect(10,y,120,15,ILI9341_BLACK);
-    tft.setFont(NULL);
-    tft.setTextSize(2);
-    tft.setTextColor(ILI9341_CYAN,ILI9341_BLACK);
-    tft.setCursor(10,y);
-    buf[0]='R'; buf[1]='I'; buf[2]='T'; buf[3]=' ';
     if (v == 0) {
-      strcpy(buf+4,"0    ");
+      strcpy(buf,"0    ");
     } else {
       if (v < 0) {
-        buf[4]='-';
+        buf[0]='-';
         v=-v;
       }
-      else buf[4]='+'; 
+      else buf[0]='+'; 
       //sprintf(buf+5,"%dHz",v);
       //itoa(v,buf+5,10);
-      cwr_str(cwr_int(buf+5,v),"Hz");
+      cwr_str(cwr_int(buf+1,v),"Hz");
       //strcat(buf+5,"Hz");
-      for (byte i=5; i<=10; i++)
+      for (byte i=1; i<=6; i++)
         if (buf[i] == 0) buf[i]=' ';
-      buf[11]=0;
+      buf[7]=0;
     }
+    tft.fillRect(10,y,90,16,ILI9341_BLACK);
+    tft.setFont(NULL);
+    tft.setTextSize(2);
+    tft.setTextColor(ILI9341_BLUE,ILI9341_BLACK);
+    tft.setCursor(10,y);
     tft.print(buf);
     tft.setTextSize(1);
   }
